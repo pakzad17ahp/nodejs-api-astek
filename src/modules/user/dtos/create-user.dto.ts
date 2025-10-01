d431f8dbf30ad5afd7ab780a65836e41cd30698d
Amir Pakzad <76836602+pakzad17ahp@users.noreply.github.com>
2025-10-01 20:10:28 +0330

    Add Dtos : 1.create-user 2.update-user Add Validator Middleware

---
+++ src/modules/user/dtos/create-user.dto.ts
@@ -0,0 +1,23 @@
+import {
+  IsNotEmpty,
+  Length,
+  IsNumberString,
+  MaxLength,
+  MinLength,
+} from "class-validator";
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  username!: string;
+
+  @IsNotEmpty()
+  name!: string;
+
+  @IsNumberString()
+  @MinLength(10)
+  @MaxLength(10)
+  phone!: string;
+
+  @Length(6, 20)
+  password!: string;
+}
---
+++ src/modules/user/dtos/update-user.dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsOptional,
+  IsNumberString,
+  MinLength,
+  MaxLength,
+  IsString,
+  IsBoolean,
+} from "class-validator";
+
+export class UpdateUserDto {
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @IsNumberString()
+  @IsOptional()
+  @MinLength(10)
+  @MaxLength(10)
+  phone?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  is_super_admin?: boolean;
+}
