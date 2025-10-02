2fb6d3b6e15a6af9090196a149c1729348bd2b03
Amir Pakzad <76836602+pakzad17ahp@users.noreply.github.com>
2025-10-02 19:05:44 +0330

    Add Account Permissions in Routes

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -4,19 +4,37 @@ import { CreateUserDto } from "./dtos/create-user.dto";
 import { UpdateUserDto } from "./dtos/update-user.dto";
 import { authMiddleware } from "../../shared/middlewares/auth.middleware";
 import { UserController } from "./user.controller";
+import {
+  action,
+  checkPermission,
+  section,
+} from "../../shared/middlewares/check-permission.middleware";
+import { accountUpdatePermission } from "../../shared/middlewares/account-update-permission.middleware";
 
 const userController = new UserController();
 const router = Router();
 
 // GET api/users
-router.get("/", userController.getAll.bind(userController));
+router.get(
+  "/",
+  authMiddleware(false),
+  checkPermission(section.account, action.view),
+  userController.getAll.bind(userController)
+);
 
 // GET api/users/:id
-router.get("/:id", userController.getById.bind(userController));
+router.get(
+  "/:id",
+  authMiddleware(false),
+  checkPermission(section.account, action.view),
+  userController.getById.bind(userController)
+);
 
 //POST api/users
 router.post(
   "/",
+  authMiddleware(false),
+  checkPermission(section.account, action.create),
   validationMiddleware(CreateUserDto),
   userController.create.bind(userController)
 );
@@ -24,7 +42,10 @@ router.post(
 // PUT api/users/:id
 router.put(
   "/:id",
+  authMiddleware(true),
+  checkPermission(section.account, action.update),
   validationMiddleware(UpdateUserDto),
+  accountUpdatePermission,
   userController.update.bind(userController)
 );
 
